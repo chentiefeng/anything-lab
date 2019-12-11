14f6cd0aec087d41d733dca1796b7d7966671cc0
chentiefeng <chentiefeng@aliyun.com>
2019-12-11 17:42:49 +0800

    增加时间正则部分

--- src/main/java/me/ctf/lab/AnythingLabApplication.java
+++ src/main/java/me/ctf/lab/AnythingLabApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.stereotype.Repository;
 
 @MapperScan("me.ctf.lab.dynamicdatasource")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
