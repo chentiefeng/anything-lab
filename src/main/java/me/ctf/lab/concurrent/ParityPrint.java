c45c0d714b3ab7ddde90b38051f1f19eee859dba
chentiefeng <chentiefeng@192.168.0.103>
2019-12-30 23:49:42 +0800

    update

--- src/main/java/me/ctf/lab/concurrent/ParityPrint.java
+++ src/main/java/me/ctf/lab/concurrent/ParityPrint.java
@@ -1,65 +1,48 @@
 package me.ctf.lab.concurrent;
 
+import java.util.Scanner;
+
 /**
- * 奇偶打印，随机传入一个正整数，通过两个线程交替+1打印奇偶数，每个线程打印3次
+ * 奇偶打印，控制台随机输入一个正整数n，通过两个线程分别打印n+1、n+2、n+3、n+4、n+5、n+6，要求线程交替打印（即一个奇数线程，一个偶数线程），n的初始指为-1
  *
  * @author: chentiefeng[chentiefeng@linzikg.com]
  * @create: 2019-12-30 17:18
  */
 public class ParityPrint {
-    private static int i = -1;
 
+    private int i = -1;
+    
     public static void main(String[] args) throws InterruptedException {
-        Thread oddThread = new Thread(new OddNumber());
-        Thread evenThread = new Thread(new EvenNumber());
+        ParityPrint parityPrint = new ParityPrint();
+        Thread oddThread = buildOddThread(parityPrint);
+        Thread evenThread = buildEvenThread(parityPrint);
         evenThread.start();
         oddThread.start();
-        System.out.println("主线程开始等待1秒");
+        //暂停1秒，等待两个线程进入wait状态
         Thread.sleep(1000);
-        System.out.println("主线程开始等待1秒结束");
+        System.out.print("请输入一个随机正整数：");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
         synchronized (ParityPrint.class) {
-            i = 45;
+            parityPrint.i = num;
             ParityPrint.class.notifyAll();
             System.out.println("主线程唤醒所有线程");
         }
+        input.close();
     }
 
     /**
-     * 奇数线程
+     * 偶数线程创建
+     * 
+     * @param parityPrint
+     * @return
      */
-    static class OddNumber implements Runnable {
-        @Override
-        public void run() {
-            for (int j = 0; j < 3; j++) {
-                synchronized (ParityPrint.class) {
-                    //小于0或者当前是奇数都等待
-                    while (i < 0 || (i & 1) == 1) {
-                        try {
-                            System.out.println("奇数线程等待中");
-                            ParityPrint.class.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println("奇数线程获得锁");
-                    }
-                    //偶数的话+1打印
-                    System.out.println("奇数线程打印：" + (++i));
-                    ParityPrint.class.notify();
-                }
-            }
-        }
-    }
-
-    /**
-     * 偶数线程
-     */
-    static class EvenNumber implements Runnable {
-        @Override
-        public void run() {
+    private static Thread buildEvenThread(ParityPrint parityPrint) {
+        return new Thread(()->{
             for (int j = 0; j < 3; j++) {
                 synchronized (ParityPrint.class) {
                     //小于0或者当前是偶数都等待
-                    while (i < 0 || (i & 1) == 0) {
+                    while (parityPrint.i < 0 || (parityPrint.i & 1) == 0) {
                         try {
                             System.out.println("偶数线程等待中");
                             ParityPrint.class.wait();
@@ -69,10 +52,38 @@ public class ParityPrint {
                         System.out.println("偶数线程获得锁");
                     }
                     //奇数的话+1打印
-                    System.out.println("偶数线程打印：" + (++i));
+                    System.out.println("偶数线程打印：" + (++parityPrint.i));
                     ParityPrint.class.notify();
                 }
             }
-        }
+        });
+    }
+
+    /**
+     * 奇数线程创建
+     * 
+     * @param parityPrint
+     * @return
+     */
+    private static Thread buildOddThread(ParityPrint parityPrint) {
+        return new Thread(()->{
+            for (int j = 0; j < 3; j++) {
+                synchronized (ParityPrint.class) {
+                    //小于0或者当前是奇数都等待
+                    while (parityPrint.i < 0 || (parityPrint.i & 1) == 1) {
+                        try {
+                            System.out.println("奇数线程等待中");
+                            ParityPrint.class.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("奇数线程获得锁");
+                    }
+                    //偶数的话+1打印
+                    System.out.println("奇数线程打印：" + (++parityPrint.i));
+                    ParityPrint.class.notify();
+                }
+            }
+        });
     }
 }
